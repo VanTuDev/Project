6d4902d820e516ccba9fcbaf8f071f1ce4d0dae8
VanTuDev <VanTu.dev@gmail.com>
2023-10-04 16:45:26 +0700

    UPDATE

--- src/java/control/Homecontrol.java
+++ src/java/control/Homecontrol.java
@@ -11,7 +11,7 @@ import entity.Product;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import javafx.scene.input.DataFormat;
+//import javafx.scene.input.DataFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -49,29 +49,14 @@ public class Homecontrol extends HttpServlet {
         throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
