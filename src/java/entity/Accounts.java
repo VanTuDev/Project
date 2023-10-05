3dd7540604ff3db12bae96d5a225a6641e526a6c
VanTuDev <VanTu.dev@gmail.com>
2023-10-05 16:29:15 +0700

    update signup

--- src/java/entity/Accounts.java
+++ src/java/entity/Accounts.java
@@ -4,6 +4,8 @@
  */
 package entity;
 
+import java.sql.Date;
+
 /**
  *
  * @author admin
@@ -15,21 +17,107 @@ public class Accounts {
     private int renter;
     private int lessor;
     private int admins;
+    private String img;
+    private String fullname;
+    private Date dob;
+    private String gender;
+    private String numphone;
+    private String cccd;
     private String gmail;
+    private String roles;
+    private String dot;
+    private String wot;
 
-    
-
-    public Accounts() {
-    }
-
-    public Accounts(String user, String pass, int renter, int lessor, int admins, String gmail) {
+    public Accounts(String user, String pass, int renter, int lessor, int admins, String img, String fullname, Date dob, String gender, String numphone, String cccd, String gmail, String roles, String dot, String wot) {
         this.user = user;
         this.pass = pass;
         this.renter = renter;
         this.lessor = lessor;
         this.admins = admins;
+        this.img = img;
+        this.fullname = fullname;
+        this.dob = dob;
+        this.gender = gender;
+        this.numphone = numphone;
+        this.cccd = cccd;
         this.gmail = gmail;
+        this.roles = roles;
+        this.dot = dot;
+        this.wot = wot;
     }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNumphone() {
+        return numphone;
+    }
+
+    public void setNumphone(String numphone) {
+        this.numphone = numphone;
+    }
+
+    public String getCccd() {
+        return cccd;
+    }
+
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getDot() {
+        return dot;
+    }
+
+    public void setDot(String dot) {
+        this.dot = dot;
+    }
+
+    public String getWot() {
+        return wot;
+    }
+
+    public void setWot(String wot) {
+        this.wot = wot;
+    }
+   
 
   
 
@@ -80,14 +168,16 @@ public class Accounts {
     public void setGmail(String gmail) {
         this.gmail = gmail;
     }
+
+    @Override
+    public String toString() {
+        return "Accounts{" + "user=" + user + ", pass=" + pass + ", renter=" + renter + ", lessor=" + lessor + ", admins=" + admins + ", img=" + img + ", fullname=" + fullname + ", dob=" + dob + ", gender=" + gender + ", numphone=" + numphone + ", cccd=" + cccd + ", gmail=" + gmail + ", roles=" + roles + ", dot=" + dot + ", wot=" + wot + '}';
+    }
     
 
  
 
-    @Override
-    public String toString() {
-        return "Accounts{" + "user=" + user + ", pass=" + pass + ", renter=" + renter + ", lessor=" + lessor + ", admins=" + admins + ", gmail=" + gmail + '}';
-    }
+   
    
 
 }
